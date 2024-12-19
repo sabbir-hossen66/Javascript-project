0304bccaca3cdd6c9848979a2f3fac04616fbc2c
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2024-12-19 18:53:16 +0600

    create js file and showing data

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
 const milestonesData = JSON.parse(data).data;
-console.log(milestonesData)
+
+// load milestone data
+
+function loadMilestones() {
+  const milestone = document.querySelector('.milestones')
+
+  milestone.innerHTML = `
+  ${milestonesData.map(function (milestone) {
+    return `<div class="milestones">
+    <div class="milestone border-b">
+      <div class="flex">
+        <div class="checkbox"><input type="checkbox" /></div>
+        <div>
+          <p>
+           ${milestone.name}
+            <span><i class="fas fa-chevron-down"></i></span>
+          </p>
+        </div>
+      </div>
+      <div class="hidden_panel">
+        <div class="module border-b">
+          <p>Module Name</p>
+        </div>
+      </div>
+    </div>
+  </div>`
+  }).join(' ')}
+  `
+}
+loadMilestones()
